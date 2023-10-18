5500cd8767eca0601899618532720f24f668d164
flaviodrummond <flaviosax20@gmail.com>
2023-10-18 09:56:49 -0300

    Ajuste de comparação dentro dos inputs

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ function buttonClick() {
     const onNumber = Math.ceil (document.querySelector(".one-number").value)
     const twoNumber = Math.floor (document.querySelector(".two-number").value)
 
-    const result = Math.floor(Math.random() * (twoNumber - onNumber + 1) + onNumber)
+    if (onNumber >= twoNumber){
+        alert("O valor mínimo não pode ser MENOR que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (twoNumber - onNumber + 1) + onNumber)
 
     alert(result)
-
+    }
+ 
 }
