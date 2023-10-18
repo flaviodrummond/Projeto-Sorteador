c9cb3c132b87edccb57ed2253f7b3859d516e9b9
flaviodrummond <flaviosax20@gmail.com>
2023-10-18 00:25:50 -0300

    Finalizado o HTML e o CSS do projeto sorteador

---
+++ script.js
@@ -0,0 +1,8 @@
+const buttonClick = document.querySelector(".click")
+
+function buttonClick(){
+
+    const inputOneNumber = document.querySelector(".one-number").value
+    const inputTwoNumber = document.querySelector(".two-number").value
+    console.log(inputOneNumber, inputTwoNumber)
+}
