1cf23279c295d01e71865211877d21888cdc7d49
flaviodrummond <flaviosax20@gmail.com>
2024-03-26 14:46:14 -0300

    Tirando o alert do resultado e colocando o resultdo direto na tela com o javascript

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const convertButton = document.querySelector('.convert-button')
 
+const resultValue = document.querySelector('.result-value')
+
 function buttonClick() {
 
     const min = Math.round (document.querySelector('.min').value)
@@ -8,8 +10,31 @@ function buttonClick() {
 
     const result = Math.round(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+    if(min === max){
+        
+    } else {
+        alert('Valor menor que o valor máximo')
+    }
 
+
+
+    const resultelement = document.createElement('div')
+
+    resultelement.classList.add('result-value')
+    resultelement.innerHTML = result
+
+    const resultValue = document.querySelector('.result-value')
+    resultValue.innerHTML = ''
+
+   
+    resultValue.append(result)
+
+    if(resultValue.style.display == 'block'){
+        resultValue.style.display = 'none'
+
+    } else {
+        resultValue.style.display = 'block'
+    }
 }
 
 
